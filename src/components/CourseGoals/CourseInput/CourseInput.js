5542b623865b81ebc98874230032591428752020
Wijayaac <wijayaac25@gmail.com>
2022-04-19 21:32:37 +0800

    fix : styled component plugin

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,33 +1,33 @@
 import React, { useState } from "react";
-import styledComponents from "styled-components";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
 
-const FormControl = styledComponents.div`
+const FormControl = styled.div`
   margin: 0.5rem 0;
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color: ${(props) => (props.invalid ? "red" : "black")}
-}
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
+  }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-  background-color: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")}
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background-color: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
 `;
 
 const CourseInput = (props) => {
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,6 @@
-import styledComponents from "styled-components";
+import styled from "styled-components";
 
-const Button = styledComponents.button`
+const Button = styled.button`
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -8,16 +8,17 @@ const Button = styledComponents.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
-  
+
   &:focus {
     outline: none;
   }
 
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}`;
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
 
 export default Button;
